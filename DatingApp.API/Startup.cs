13739bc314d30b0c4323320963000381c008dd5b
Farhan Kaioum <km1472233@gmail.com>
2020-10-24 21:55:16 +0600

    Initial commit

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -22,13 +22,11 @@ namespace DatingApp.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,6 +38,7 @@ namespace DatingApp.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
